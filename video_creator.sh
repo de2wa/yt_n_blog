a02556b6c61a9f43ca941a690a1490cfda03744d
de2wa <de2wa.net@gmail.com>
2022-10-04 06:16:29 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -10,24 +10,23 @@ do
     ffmpeg -i "https://de2wa.com/video/intro$1.mp4"  -t 10 -crf $(( ( RANDOM % 28)  + 12 )) -b:v $(( ( RANDOM % 6)  + 2 ))  -vf "eq = brightness = 0.0$i" -vf "eq = contrast = 0.8$i"  "intro$i.mp4"
 done
 
-for (( i=0 ; i<=9 ; i++ )); 
+
+==============================
+for (( i=1 ; i<=10 ; i++ )); 
 do
-    ffmpeg -ss 00:0$i:00 -to 00:3$i:00  -i "https://de2wa.com/video/input2.mp4"  -acodec copy  -vcodec copy  -f mpegts "input$i.ts"
+    ffmpeg  -i "https://de2wa.com/video/intro.mp4"  -acodec copy  -vcodec copy  -f mpegts "intro$i.ts"
 done
 
-
+ffmpeg -i "input.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input.ts"
 
 ffmpeg -i "https://de2wa.com/video/introclick.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
 
 
-for (( i=1 ; i<=20 ; i++ )); 
-do
-    ffmpeg -i "intro$i.mp4" -acodec copy -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
-done
+
 
 mkdir aajadi
 
 for (( i=1 ; i<=20 ; i++ )); 
 do
-    ffmpeg -i "concat:intro$i.ts|introclick.ts|input$(( ( RANDOM % 9)  + 1 )).ts|input$(( ( RANDOM % 9)  + 1 )).ts|intro$(( ( RANDOM % 20)  + 1 )).ts|intro$(( ( RANDOM % 20)  + 1 )).ts|intro$(( ( RANDOM % 20)  + 1 )).ts|input$(( ( RANDOM % 9)  + 1 )).ts||input$(( ( RANDOM % 9)  + 1 )).ts|intro$(( ( RANDOM % 20)  + 1 )).ts" -acodec copy -vcodec copy  "aajadi/videofilviral$i.mp4"
+    ffmpeg -i "concat:intro$i.ts|introclick.ts||intro$(( ( RANDOM % 10)  + 1 )).ts" -acodec copy -vcodec copy  "aajadi/videofilviral$i.mp4"
 done
