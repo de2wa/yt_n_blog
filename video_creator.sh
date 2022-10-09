15fa4b137ae8deb45c692d91af285740bd3cee80
de2wa <de2wa.net@gmail.com>
2022-10-09 15:10:51 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -4,47 +4,21 @@
 #https://shell.cloud.google.com/?hl=en_US&fromcloudshell=true&show=ide%2Cterminal
 #docker run -p 6070:80 dorowu/ubuntu-desktop-lxde-vnc
 
-sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp  
+#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+#sudo chmod a+rx /usr/local/bin/yt-dlp  
 
 
-rm image*.mp4
-rm image/*.ts
-rm image/*.mp4
-rm image/*.jpg
-
-yt-dlp -f 22 https://www.youtube.com/watch?v=jkAcNHwM_4k -o image1.mp4
-
-mkdir image 
-
-ffmpeg -i image1.mp4 -vf fps=1/10 image/img%03d.jpg
-
-
-rm image*.mp4
-
-for (( i=10 ; i<=99; i++ )); 
-do
-    ffmpeg -framerate 30 -loop 1 -i image/img0$i.jpg -t 00:00:02 -vf format=yuv420p image/video$i.mp4
-done
-
-for (( i=10 ; i<=99; i++ )); 
-do
-    ffmpeg -i image/video$i.mp4  -acodec copy -vcodec copy -vbsf h264_mp4toannexb -f mpegts image/video$i.ts
-
-done
-
+#yt-dlp -f 22 https://www.youtube.com/watch?v=jkAcNHwM_4k -o image1.mp4
 
 ffmpeg -i "https://de2wa.com/video/introclick.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
+ffmpeg -i "https://de2wa.com/video/input.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input.ts"
 
-for (( f=1 ; f<=20; f++ )); 
+for (( f=1 ; f<=40; f++ )); 
 do
-rm mylist.txt 
+ffmpeg -i "https://de2wa.com/video/intro$f.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$f.ts"
+done
 
-echo "file 'introclick.ts'" >> mylist.txt
-
-for (( i=1 ; i<=$(shuf -i 2500-3600 -n 1); i++ ));  do echo "file 'image/video$(shuf -i 10-99 -n 1).ts'" >> mylist.txt; done
-
-ffmpeg -f concat -i mylist.txt -vcodec copy viral_movie_video$f.mp4
-
+for (( i=1 ; i<=30; i++ ));  do 
+ffmpeg -i "concat:intro$$(shuf -i 1-40 -n 1).ts|introclick.ts|input.ts|intro$$(shuf -i 1-30 -n 1).ts" -vcodec copy viral_movie_video$f.mp4
 done
 
