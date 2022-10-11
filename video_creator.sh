6f4f77f2dfb6528193264d92616c0c4093fbbb64
de2wa <de2wa.net@gmail.com>
2022-10-12 00:16:10 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -2,23 +2,30 @@
 #!/bin/bash  
 
 #https://shell.cloud.google.com/?hl=en_US&fromcloudshell=true&show=ide%2Cterminal
-#docker run -p 6070:80 dorowu/ubuntu-desktop-lxde-vnc
-
-#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-#sudo chmod a+rx /usr/local/bin/yt-dlp  
+#docker run -p 6070:80  -e VNC_PASSWORD=root1234 hdavid0510/ubuntu-desktop-lxde-vnc
 
 
-#yt-dlp -f 22 https://www.youtube.com/watch?v=jkAcNHwM_4k -o image1.mp4
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp  
+
+rm *.ts
+rm *.mp4
+yt-dlp -f 22 https://www.youtube.com/watch?v=jkAcNHwM_4k -o input1.mp4
+yt-dlp -f 22 https://www.youtube.com/watch?v=jkAcNHwM_4k -o input2.mp4
+
 
 ffmpeg -i "https://de2wa.com/video/introclick.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
-ffmpeg -i "https://de2wa.com/video/input.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input.ts"
 
-for (( f=1 ; f<=40; f++ )); 
-do
-ffmpeg -i "https://de2wa.com/video/intro$f.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$f.ts"
+for (( i=1 ; i<=10; i++ ));  do 
+ffmpeg -i "input1.mp4" -ss 00:(shuf -i 10-59 -n 1):00 -t 00:(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
+done
+for (( i=1 ; i<=10; i++ ));  do 
+ffmpeg -i "input2.mp4" -ss 00:0(shuf -i 1-9 -n 1):00 -t 00:0(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
 done
 
-for (( i=1 ; i<=30; i++ ));  do 
-ffmpeg -i "concat:intro$$(shuf -i 1-40 -n 1).ts|introclick.ts|input.ts|intro$$(shuf -i 1-30 -n 1).ts" -vcodec copy viral_movie_video$f.mp4
+
+
+for (( i=1 ; i<=40; i++ ));  do 
+ffmpeg -i "concat:introclick.ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts" -acodec copy -vcodec copy viral_movie_video$f.mp4
 done
 
