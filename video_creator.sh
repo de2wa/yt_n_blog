6c2e83ab6848295db22b4ae8c46c243c9ef9b9d3
de2wa <de2wa.net@gmail.com>
2022-10-12 01:12:14 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -17,10 +17,10 @@ yt-dlp -f 22 https://www.youtube.com/watch?v=5BnfqRmzHC8 -o input1.mp4
 ffmpeg -i "https://de2wa.com/video/introclick.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
 
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "input1.mp4" -ss 00:(shuf -i 10-59 -n 1):00 -t 00:(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
+ffmpeg -i "input1.mp4" -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "input2.mp4" -ss 00:0(shuf -i 1-9 -n 1):00 -t 00:0(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
+ffmpeg -i "input2.mp4" -ss 00:0$(shuf -i 1-9 -n 1):00 -t 00:0$(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
 done
 
 
