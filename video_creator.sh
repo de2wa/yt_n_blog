9e732848b47ef1a691c7ddfc86d8ed1c4782e216
de2wa <de2wa.net@gmail.com>
2022-10-13 07:23:23 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -20,7 +20,7 @@ for (( i=1 ; i<=10; i++ ));  do
 ffmpeg -i "https://de2wa.com/video/input2.mp4" -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "input1.mp4" -ss 00:0$(shuf -i 1-9 -n 1):00 -t 00:0$(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
+ffmpeg -i "input1.mp4" -t 00:0$(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
 done
 
 
