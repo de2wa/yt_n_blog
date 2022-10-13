e7d9285d6285883d214ac8d6164e03f7af5b55fa
de2wa <de2wa.net@gmail.com>
2022-10-13 07:17:42 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -2,7 +2,7 @@
 #!/bin/bash  
 
 #https://shell.cloud.google.com/?hl=en_US&fromcloudshell=true&show=ide%2Cterminal
-#docker run -p 6070:80  -e VNC_PASSWORD=root1234 hdavid0510/ubuntu-desktop-lxde-vnc
+#docker run -p 6070:80 dorowu/ubuntu-desktop-lxde-vnc
 
 
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
@@ -10,14 +10,14 @@ sudo chmod a+rx /usr/local/bin/yt-dlp
 
 rm *.ts
 rm *.mp4
-yt-dlp -f 22 https://www.youtube.com/watch?v=MkX43WNgfes -o input2.mp4
-yt-dlp -f 22 https://www.youtube.com/watch?v=5BnfqRmzHC8 -o input1.mp4
+yt-dlp -f 22 https://www.youtube.com/watch?v=MkX43WNgfes -o input1.mp4
+#yt-dlp -f 22 https://www.youtube.com/watch?v=5BnfqRmzHC8 -o input1.mp4
 
 
 ffmpeg -i "https://de2wa.com/video/introclick.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
 
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "input1.mp4" -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
+ffmpeg -i "https://de2wa.com/video/input2.mp4" -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 for (( i=1 ; i<=10; i++ ));  do 
 ffmpeg -i "input2.mp4" -ss 00:0$(shuf -i 1-9 -n 1):00 -t 00:0$(shuf -i 5-9 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
