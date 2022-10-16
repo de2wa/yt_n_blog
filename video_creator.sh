c258fdb33c72e7eb67301def78f9892d566112cb
de2wa <de2wa.net@gmail.com>
2022-10-16 08:02:18 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -10,7 +10,7 @@ sudo chmod a+rx /usr/local/bin/yt-dlp
 
 rm *.ts
 rm *.mp4
-yt-dlp -f best https://www.youtube.com/watch?v=cGhDjDwyBr0 -o input1.mp4
+#yt-dlp -f best https://www.youtube.com/watch?v=cGhDjDwyBr0 -o input1.mp4
 #yt-dlp -f 22 https://www.youtube.com/watch?v=5BnfqRmzHC8 -o input1.mp4
 
 
@@ -20,7 +20,7 @@ for (( i=1 ; i<=10; i++ ));  do
 ffmpeg -i "https://de2wa.com/video/input2.mp4" -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "input1.mp4" -t 00:0$(shuf -i 5-9 -n 1):00 -an  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
+ffmpeg -i "https://de2wa.com/video/newvideo.mp4" -t 00:0$(shuf -i 5-9 -n 1):00 -an  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
 done
 
 
