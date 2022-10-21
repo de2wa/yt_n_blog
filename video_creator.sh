e5caeadb7da70998c0e83f76c848de2258c35179
de2wa <de2wa.net@gmail.com>
2022-10-21 22:34:59 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -10,23 +10,25 @@ sudo chmod a+rx /usr/local/bin/yt-dlp
 
 rm *.ts
 rm *.mp4
-#yt-dlp -f best https://www.youtube.com/watch?v=cGhDjDwyBr0 -o input1.mp4
-#yt-dlp -f 22 https://www.youtube.com/watch?v=5BnfqRmzHC8 -o input1.mp4
+yt-dlp -f best https://www.youtube.com/watch?v=h749ShyfnWE -o music.mp4
+yt-dlp -f 22 https://www.youtube.com/watch?v=WQmGwmc-XUY -o intro.mp4
 
+ffmpeg -i music.mp4 -acodec copy music.mp3
+ffmpeg -i intro.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb -f mpegts intro.ts
 
 ffmpeg -i "https://de2wa.com/video/introclick1.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
 
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "https://de2wa.com/video/input2.mp4" -i audio.wav  -ss 00:$(shuf -i 10-59 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -map 0:v -map 1:a -c:v copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
+ffmpeg -i "https://de2wa.com/video/input2.mp4" -i music.mp3  -ss 00:0$(shuf -i 5-9 -n 1):00 -t 00:$(shuf -i 20-40 -n 1):00 -map 0:v -map 1:a -c:v copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "https://de2wa.com/video/input2.mp4" -t 00:0$(shuf -i 5-9 -n 1):00 -vf "eq=brightness=0.02, eq=contrast=0.$(shuf -i 5-8 -n 1)" -preset slow -crf 18 -b:v $(shuf -i 3-6 -n 1).5M   -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
+ffmpeg -i "intro.mp4" -t 00:00:$(shuf -i 10-19 -n 1) -vf "eq=brightness=0.02, eq=contrast=0.$(shuf -i 5-8 -n 1)" -preset slow -crf 18 -b:v $(shuf -i 3-6 -n 1).5M  -acodec copy -vcodec copy -vbsf h264_mp4toannexb -f mpegts "intro$i.ts"
 done
 
 
 mkdir aajadi 
 
-for (( i=1 ; i<=100; i++ ));  do 
-ffmpeg -i "concat:introclick.ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts" -acodec copy -vcodec copy aajadi/kHGGhy$i.mp4
+for (( i=1 ; i<=50; i++ ));  do 
+ffmpeg -i "concat:intro$(shuf -i 1-10 -n 1).ts|introclick.ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts" -acodec copy -vcodec copy aajadi/kHGGhy$i.mp4
 done
 
