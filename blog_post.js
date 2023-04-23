f924df615d72a6ee3fffa7d38f75adf26268aa35
de2wa <de2wa.net@gmail.com>
2023-04-23 09:35:52 +0700

    Update blog_post.js

--- blog_post.js
+++ blog_post.js
@@ -1,4 +1,4 @@
-function mainFunction() 
+function mainFunction1() 
 {
   // *************************************************************************
   // CODE WITH CURT 2/14/2021
@@ -7,76 +7,77 @@ function mainFunction()
   // are searched in the RegExp statements.
   // *************************************************************************
 
-  //clearRecords()
+  //clearRecords();
+
+ var cat = ["islamic+wallpaper+desktop", "islamic+wallpaper+hd", "Allah+wallpaper+top", "islamic+wallpaper"];
+var kw  = cat[Math.floor(Math.random() * cat.length)];
+
+var email = ["prenticeprescott.blog5@blogger.com","prenticeprescott.blog4@blogger.com","prenticeprescott.blog3@blogger.com","prenticeprescott.blog2@blogger.com","prenticeprescott.blog1@blogger.com"];
+var dataEmail  = email[Math.floor(Math.random() * email.length)];
+
+//Logger.log(kw);
+ var rand = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  var url = "https://www.bing.com/images/search?q=" + kw + "&form=HDRSC2&first=" + rand + "&tsc=ImageHoverTitle";
+  var str = UrlFetchApp.fetch(url).getContentText();
+  var data1 = str.replace(/&quot;/g, '"');
+  var data = data1.replace(/[^\x00-\x7F]/g, "");
 
   
-  //var values = SpreadsheetApp.getSheetByName("TABLE").getRange("A:C").getValues();
 
-  var ss= SpreadsheetApp.getActiveSpreadsheet();
-  var tableSheet1 = ss.getSheetByName("Sheet1");
-  var judulsheet = tableSheet1.getRange("C:C").getValues();
-  var dessheet = tableSheet1.getRange("D:D").getValues();
-  var imagesheet = tableSheet1.getRange("B:B").getValues();
-  //var values1 = tableSheet1.getRange(1,1,tableSheet1.getLastRow(), 4); 
-    //var datajudul1 = judulsheet.replace(/[^\x00-\x7F]/g, "");
+  const mainRegex = /<div class="iuscp([\s\S]*?)}" onclick="/gm
+  var results = data.match(mainRegex);
+//Logger.log(results);
+  const image = /(?<="murl":").*?(?=")/gi;
+  const title = /(?<="t":").*?(?=")/gi;
+  const desc = /(?<="desc":").*?(?=")/gi;
 
-  var datajudul = judulsheet.filter(String);
-  var datades = dessheet.filter(String);
-  var dataimage = imagesheet.filter(String);
-  
-  var emailSheet1 = ss.getSheetByName("Sheet2");
-  var emails = emailSheet1.getRange("A:A").getValues();
-  var dataEmail = emails.filter(String);
-  //Logger.log(dataemail[0]);
-for(var k = 0; k < dataEmail.length; k++)
+  //const teamLogoPass = /<img alt="([\s\S]*?)" class="hCL kVc L4E MIw"/gi;
+//var results = results1.slice(20,40);
+for(var i = 0; i < 19; i++)
   {
-  var data = datajudul.length;
-  var max = data - 20;
-  var num = Math.floor(Math.random() * (max - 1 + 1)) + 1;
-
-  for(var j = num; j < num + 10; j++)
-    {
-      var random = Math.floor(Math.random() * (30)) + 19;
     //Logger.log('content: ' + results[i]);
-    var judulblog = "[" + random + "+]" + datajudul[j] + " " + datajudul[num];
-    var deskripsiblog1 = datades[j+1];
-    var deskripsiblog2 = datades[j+2];
-    var deskripsiblog3 = datades[j+3];
-    var deskripsiblog4 = datades[j+4];
-    var deskripsiblog5 = datades[j+5];
-    var imageblog = dataimage[j];
-    var konten2 = ["<h3>" + datajudul[j] + "</h3> <img alt='" + datajudul[j] + "' src='" + dataimage[j] + "' width='75%'/><p>" + datades[j] + "</p><!--more-->"] ;
-    var konten3 = ["<h3>" + datajudul[j+1] + "</h3> <img alt='" + datajudul[j+1] + "' src='" + dataimage[j+1] + "' width='75%'/><p>" + datades[j+1] + "</p>"] ;
-   var konten4 = ["<h3>" + datajudul[j+2] + "</h3> <img alt='" + datajudul[j+2] + "' src='" + dataimage[j+2] + "' width='75%'/><p>" + datades[j+2] + "</p>"] ;
-   var konten5 = ["<h3>" + datajudul[j+3] + "</h3> <img alt='" + datajudul[j+3] + "' src='" + dataimage[j+3] + "' width='75%'/><p>" + datades[j+3] + "</p>"] ;
-   var konten6 = ["<h3>" + datajudul[j+4] + "</h3> <img alt='" + datajudul[j+4] + "' src='" + dataimage[j+4] + "' width='75%'/><p>" + datades[j+4] + "</p>"] ;
-   var konten7 = ["<h3>" + datajudul[j+5] + "</h3> <img alt='" + datajudul[j+5] + "' src='" + dataimage[j+5] + "' width='75%'/><p>" + datades[j+5] + "</p>"] ;
-   var konten8 = ["<h3>" + datajudul[j+6] + "</h3> <img alt='" + datajudul[j+6] + "' src='" + dataimage[j+6] + "' width='75%'/><p>" + datades[j+6] + "</p>"] ;
-   var konten9 = ["<h3>" + datajudul[j+7] + "</h3> <img alt='" + datajudul[j+7] + "' src='" + dataimage[j+7] + "' width='75%'/><p>" + datades[j+7] + "</p>"] ;
-   var konten10 = ["<h3>" + datajudul[j+8] + "</h3> <img alt='" + datajudul[j+8] + "' src='" + dataimage[j+8] + "' width='75%'/><p>" + datades[j+8] + "</p>"] ;
-    }
-  var judul = judulblog;
-    var konten1 = deskripsiblog5 + " <b>" +judulblog + "</b> " + deskripsiblog1 + " " + deskripsiblog3 + " " + deskripsiblog4 + ".";
-    var konten11 = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>  		<script type='text/javascript' src='https://cdnx.de2wa.com/asset/image.js.php'/></script>";
-    var konten = konten1 + konten2 + konten3 + konten6 + konten7 + konten8 + konten9 + konten10 + konten11;
-//try {
- // SpreadsheetApp.doThing()
-    Logger.log('judul: ' + judul);
-//Logger.log('des: ' + deskripsiblog1);
-//Logger.log('konten: ' + konten);
-//Logger.log('image: ' + imageblog);
+   var gambar1 = '<h3>'+results[0].match(title)+'</h3><img src="'+results[0].match(image)+'" alt="'+results[0].match(title)+'"> <p>'+results[0].match(title)+' '+results[0].match(desc)+'</p>';
+  var gambar2 = '<h3>'+results[1].match(title)+'</h3><img src="'+results[1].match(image)+'" alt="'+results[1].match(title)+'"> <p>'+results[1].match(title)+' '+results[1].match(desc)+'</p>';
+  var gambar3 = '<h3>'+results[2].match(title)+'</h3><img src="'+results[2].match(image)+'" alt="'+results[2].match(title)+'"> <p>'+results[2].match(title)+' '+results[2].match(desc)+'</p>';
+  var gambar4 = '<h3>'+results[3].match(title)+'</h3><img src="'+results[3].match(image)+'" alt="'+results[3].match(title)+'"> <p>'+results[3].match(title)+' '+results[3].match(desc)+'</p>';
+  var gambar5 = '<h3>'+results[4].match(title)+'</h3><img src="'+results[4].match(image)+'" alt="'+results[4].match(title)+'"> <p>'+results[4].match(title)+' '+results[4].match(desc)+'</p>';
+  var gambar6 = '<h3>'+results[5].match(title)+'</h3><img src="'+results[5].match(image)+'" alt="'+results[5].match(title)+'"> <p>'+results[5].match(title)+' '+results[5].match(desc)+'</p>';
+  var gambar7 = '<h3>'+results[6].match(title)+'</h3><img src="'+results[6].match(image)+'" alt="'+results[6].match(title)+'"> <p>'+results[6].match(title)+' '+results[6].match(desc)+'</p>';
+  var gambar8 = '<h3>'+results[7].match(title)+'</h3><img src="'+results[7].match(image)+'" alt="'+results[7].match(title)+'"> <p>'+results[7].match(title)+' '+results[7].match(desc)+'</p>';
+  var gambar9 = '<h3>'+results[8].match(title)+'</h3><img src="'+results[8].match(image)+'" alt="'+results[8].match(title)+'"> <p>'+results[8].match(title)+' '+results[8].match(desc)+'</p>';
+  var gambar10 = '<h3>'+results[9].match(title)+'</h3><img src="'+results[9].match(image)+'" alt="'+results[9].match(title)+'"> <p>'+results[9].match(title)+' '+results[9].match(desc)+'</p>';
+  var gambar11 = '<h3>'+results[10].match(title)+'</h3><img src="'+results[10].match(image)+'" alt="'+results[10].match(title)+'"> <p>'+results[10].match(title)+' '+results[10].match(desc)+'</p>';
+  var gambar12 = '<h3>'+results[11].match(title)+'</h3><img src="'+results[11].match(image)+'" alt="'+results[11].match(title)+'"> <p>'+results[11].match(title)+' '+results[11].match(desc)+'</p>';
+var gambar13 = '<h3>'+results[12].match(title)+'</h3><img src="'+results[12].match(image)+'" alt="'+results[12].match(title)+'"> <p>'+results[12].match(title)+' '+results[12].match(desc)+'</p>';
+
+var gambar14 = '<h3>'+results[13].match(title)+'</h3><img src="'+results[13].match(image)+'" alt="'+results[13].match(title)+'"> <p>'+results[13].match(title)+' '+results[13].match(desc)+'</p>';
+
+var gambar15 = '<h3>'+results[14].match(title)+'</h3><img src="'+results[14].match(image)+'" alt="'+results[14].match(title)+'"> <p>'+results[14].match(title)+' '+results[14].match(desc)+'</p>';
 
 
-    //var mail = dataemail[k];
-    Logger.log(dataEmail[k]); 
 
-    MailApp.sendEmail({
-      to: "" + dataEmail[k] + ""
-      , subject: judul
-      , htmlBody: konten
-    });
-//} catch (e) {
-  // ignore
-//}
+
+    //Logger.log('content: ' + gambar);
+    var judul = results[i].match(title);
+    //Logger.log('content: ' + title);
+
+    var deskripsi = results[i].match(desc);
+    //Logger.log('scrape: ' + judul);
+
   }
+
+       var random = Math.floor(Math.random() * (50)) + 19;
+ 
+var titlepost = random+'+ '+ judul[0];
+var deskripsi = gambar15 + gambar2 + gambar3 +gambar4 +gambar5 +gambar6 +gambar7 +gambar8 + gambar9 + gambar10 +gambar11+gambar12 +gambar13 +gambar14 + gambar1;
+
+
+
+Logger.log(dataEmail + titlepost);
+MailApp.sendEmail({
+      to: "" + dataEmail + ""
+      , subject: titlepost
+      , htmlBody: deskripsi
+    });
+
 }
