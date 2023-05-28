e9779703aca95802c243fbbd75b775a20c1115e5
de2wa <de2wa.net@gmail.com>
2023-05-28 21:57:59 +0700

    Update video_creator.sh

--- video_creator.sh
+++ video_creator.sh
@@ -5,30 +5,26 @@
 #docker run -p 6070:80 dorowu/ubuntu-desktop-lxde-vnc
 
 
-sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp  
+#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+#sudo chmod a+rx /usr/local/bin/yt-dlp  
 
 rm *.ts
-rm *.mp4
-yt-dlp -f best https://www.youtube.com/watch?v=h749ShyfnWE -o music.mp4
-yt-dlp -f 22 https://www.youtube.com/watch?v=WQmGwmc-XUY -o intro.mp4
+#rm *.mp4
+#yt-dlp -f best https://www.youtube.com/watch?v=h749ShyfnWE -o music.mp4
+#yt-dlp -f 22 https://www.youtube.com/watch?v=WQmGwmc-XUY -o intro.mp4
 
-ffmpeg -i music.mp4  music.mp3
+#ffmpeg -i music.mp4  music.mp3
 ffmpeg -i intro.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb -f mpegts intro.ts
+ffmpeg -i video.mp4  -t 00:00:15 -vcodec copy -acodec copy -vbsf h264_mp4toannexb -f mpegts introclick.ts
 
-ffmpeg -i "https://de2wa.com/video/introclick1.mp4"  -acodec copy  -vcodec copy -vbsf h264_mp4toannexb -f mpegts "introclick.ts"
-
-for (( i=1 ; i<=10; i++ ));  do 
-ffmpeg -i "https://de2wa.com/video/input2.mp4"  -ss 00:00:0$(shuf -i 5-9 -n 1) -t 00:$(shuf -i 20-30 -n 1):00  -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
+for (( i=1 ; i<=5; i++ ));  do 
+ffmpeg -i video.mp4  -ss 00:00:0$(shuf -i 5-9 -n 1) -t 00:$(shuf -i 20-30 -n 1):00  -vbsf h264_mp4toannexb -f mpegts "input$i.ts"
 done
 
-for (( i=1 ; i<=10; i++ ));  do 
+for (( i=1 ; i<=5; i++ ));  do 
 ffmpeg -i "intro.mp4" -t 00:00:0$(shuf -i 5-9 -n 1)  -vcodec copy -acodec copy -vbsf h264_mp4toannexb  -f mpegts "intro$i.ts"
 done
 
-
-mkdir aajadi 
-
-for (( i=1 ; i<=50; i++ ));  do 
-ffmpeg  -i "concat:intro$(shuf -i 1-10 -n 1).ts|introclick.ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|intro$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts|input$(shuf -i 1-10 -n 1).ts" -i music.mp3 -c copy -map 0:v -map 1:a aajadi/kHGGhy$i.mp4
+for (( i=1 ; i<=5; i++ ));  do 
+ffmpeg  -i "concat:intro$(shuf -i 1-5 -n 1).ts|introclick.ts|intro$(shuf -i 1-5 -n 1).ts|input$(shuf -i 1-5 -n 1).ts|input$(shuf -i 1-5 -n 1).ts|intro$(shuf -i 1-5 -n 1).ts|input$(shuf -i 1-5 -n 1).ts|input$(shuf -i 1-5 -n 1).ts" -i music.mp3 -c copy -map 0:v -map 1:a okjadi$i.mp4
 done
